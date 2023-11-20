7bfcd2e305f45612d0dcc4ae2744f5e7336883e5
tostu <torge.stubbe@googlemail.com>
2023-11-20 22:42:33 +0100

    personals

---
+++ src/lib/components/Personals.svelte
@@ -0,0 +1,27 @@
+<script>
+    import Icon from '@iconify/svelte';
+
+    const personalDataList = [
+        {icon:"user", topic:"Name", value:"Torge Stubbe"},
+        {icon:"cake", topic:"Geburtstag", value:"23.07.2000"},
+        {icon:"pin", topic:"Standort", value:"Altes Land, Germany"},
+        {icon:"briefcase", topic:"Arbeit", value:"Fricke Holding GmbH"}
+    ]
+
+</script>
+
+<div class="card w-80 bg-base-200 shadow-xl">
+    <div class="card-body">
+        {#each personalDataList as entry}
+            <div class="flex flex-row items-center text-start">
+                <div class="mr-5">
+                    <Icon icon="pixelarticons:{entry.icon}" height="auto" />
+                </div>
+                <div class="flex flex-col">
+                    <span>{entry.topic}</span>
+                    <span>{entry.value}</span>
+                </div>
+            </div>
+        {/each}
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,24 @@
 <script>
     import avatar from "$lib/assets/images/tostu_avatar.png"
+    import Personals from "$lib/components/Personals.svelte";
+
 </script>
 
-<div class="px-6 py-24 sm:py-32 lg:px-8">
-    <div class="mx-auto max-w-2xl text-center flex flex-col items-center justify-center  gap-y-2.5">
-        <img class="inline-block h-40 w-40 rounded-full" src={avatar} alt="avatar">
-        <h1 class="text-7xl font-bold tracking-tight text-gray-900">tostu</h1>
-        <h3 class="text-3xl">Full Stack Developer</h3>
+<div class="container mx-auto">
+    <div class="">
+        <div class="mx-auto max-w-2xl text-center flex flex-col items-center justify-center space-y-10">
+            <div class="space-y-3">
+                <img class="inline-block h-40 w-40 rounded-full" src={avatar} alt="avatar">
+                <h1 class="text-7xl font-bold tracking-tight text-gray-900">tostu</h1>
+                <h3 class="text-3xl">Full Stack Developer</h3>
+            </div>
+
+            <Personals></Personals>
+        </div>
+
     </div>
 </div>
+
 
 <style>
     h1,h3{
