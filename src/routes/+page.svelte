619502f8743246871994ff58544711b329c737a9
tostu <torge.stubbe@googlemail.com>
2023-11-20 17:48:06 +0100

    daisy theme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import avatar from "$lib/assets/images/tostu_avatar.png"
+</script>
+
+<div class="px-6 py-24 sm:py-32 lg:px-8">
+    <div class="mx-auto max-w-2xl text-center flex flex-col items-center justify-center  gap-y-2.5">
+        <img class="inline-block h-40 w-40 rounded-full" src={avatar} alt="avatar">
+        <h1 class="text-7xl font-bold tracking-tight text-gray-900">tostu</h1>
+        <h3 class="text-3xl">Full Stack Developer</h3>
+    </div>
+</div>
+
+<style>
+    h1,h3{
+        color: rgba(255, 255, 245, .86);
+    }
+</style>
