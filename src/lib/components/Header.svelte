3b36604548fee50d07f76b372610f7c4c54808e7
tostu <torge.stubbe@googlemail.com>
2023-11-20 20:16:21 +0100

    Header added

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,12 @@
+<script>
+    const navLinks = [
+        {label:"HOME", link:"/"},
+        {label:"SKILLS", link:"/skills"}
+    ]
+</script>
+
+<div class="navbar justify-center">
+    {#each navLinks as entry}
+        <a class="btn btn-sm btn-ghost text-xl" href={entry.link}>{entry.label}</a>
+    {/each}
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,15 @@
     import "../app.pcss"
     import '@fontsource/press-start-2p';
     import Footer from "$lib/components/Footer.svelte";
+    import Header from "$lib/components/Header.svelte";
 </script>
 
-<div class="container mx-auto min-h-screen">
+<div class="min-h-screen flex flex-col justify-between">
+    <Header></Header>
     <slot></slot>
+    <Footer></Footer>
 </div>
-<Footer></Footer>
+
 <style>
 
 
