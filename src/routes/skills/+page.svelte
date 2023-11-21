40b0e3b6f924fe6948df7798ea13637c614ff763
torge <torge.stubbe@fricke.de>
2023-11-21 15:26:44 +0100

    skill grid center

--- src/routes/skills/+page.svelte
+++ src/routes/skills/+page.svelte
@@ -49,32 +49,33 @@
 <svelte:head>
     <title>skills</title>
 </svelte:head>
-
-<div class="max-w-screen-md mx-auto my-10 ml-2 mr-5">
-    <ul class="timeline timeline-snap-icon timeline-compact timeline-vertical">
-        {#each skillPerYearList as entry}
-            <li>
-                <div class="timeline-middle">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
-                </div>
-                <div class="timeline-start mb-10">
-                    <time class="font-mono italic">{entry.year}</time>
-                    <div class="grid grid-cols-2 md:grid-cols-4 gap-2">
-                        {#each entry.skillList as skill}
-                            <div class="card w-full bg-[#202127] shadow-xl">
-                                <figure class="px-5 pt-5">
-                                    <Icon icon="{skill.icon}" width="100%" />
-                                </figure>
-                                <div class="card-body items-center text-center">
-                                    <h2 class="card-title">{skill.name}</h2>
-                                </div>
-                            </div>
-                        {/each}
+<div class="container mx-auto flex justify-center">
+    <div class="max-w-screen-md my-10 ml-2 mr-5">
+        <ul class="timeline timeline-snap-icon timeline-compact timeline-vertical">
+            {#each skillPerYearList as entry}
+                <li>
+                    <div class="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
                     </div>
-                </div>
-                <hr/>
-            </li>
-        {/each}
-    </ul>
+                    <div class="timeline-start mb-10">
+                        <time class="font-mono italic">{entry.year}</time>
+                        <div class="grid grid-cols-2 md:grid-cols-4 gap-2">
+                            {#each entry.skillList as skill}
+                                <div class="card w-full bg-[#202127] shadow-xl">
+                                    <figure class="px-5 pt-5">
+                                        <Icon icon="{skill.icon}" width="100%" />
+                                    </figure>
+                                    <div class="card-body items-center text-center">
+                                        <h2 class="card-title">{skill.name}</h2>
+                                    </div>
+                                </div>
+                            {/each}
+                        </div>
+                    </div>
+                    <hr/>
+                </li>
+            {/each}
+        </ul>
+    </div>
 </div>
 
