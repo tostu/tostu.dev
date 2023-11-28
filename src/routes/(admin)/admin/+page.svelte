c6cee9b95333f02412c13a4192061c5687796b28
tostu <torge.stubbe@googlemail.com>
2023-11-29 00:27:55 +0100

    cdn fix

--- src/routes/(admin)/admin/+page.svelte
+++ src/routes/(admin)/admin/+page.svelte
@@ -1,19 +1,13 @@
 <script>
-    import { browser } from '$app/environment';
-    import CMS from '@staticcms/core';
-    // import '@staticcms/core/dist/main.css';
     import config from "./config/config.ts";
-
-    if (browser) {
+    const initializeCMS = () => {
         window.CMS.init({config});
     }
-
-    // Now the registry is available via the CMS object.
 </script>
 
 <svelte:head>
-    meta charset="utf-8" />
+    <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="https://unpkg.com/@staticcms/app@^3.0.0/dist/main.css" />
-    <script src="https://unpkg.com/@staticcms/app@^3.0.0/dist/static-cms-app.js"></script>
+    <script src="https://unpkg.com/@staticcms/app@^3.0.0/dist/static-cms-app.js" on:load={initializeCMS}></script>
 </svelte:head>
