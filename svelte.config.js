7f4bd7cf220136db1fa4cf7c4538f75a55cb8d48
tostu <torge.stubbe@googlemail.com>
2023-11-28 21:54:53 +0100

    test

--- svelte.config.js
+++ svelte.config.js
@@ -16,9 +16,8 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: '404.html',
 			precompress: true,
-			strict: true
+			strict: false
 		})
 	}
 };
