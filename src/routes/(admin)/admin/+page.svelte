e0f2fc40a53c8af67c69e8a802168f9c60647feb
tostu <torge.stubbe@googlemail.com>
2023-11-28 22:49:04 +0100

    staticcms cdn instead bundle for build performance

--- src/routes/(admin)/admin/+page.svelte
+++ src/routes/(admin)/admin/+page.svelte
@@ -1,12 +1,19 @@
 <script>
     import { browser } from '$app/environment';
     import CMS from '@staticcms/core';
-    import '@staticcms/core/dist/main.css';
+    // import '@staticcms/core/dist/main.css';
     import config from "./config/config.ts";
 
     if (browser) {
-        CMS.init({config});
+        window.CMS.init({config});
     }
 
     // Now the registry is available via the CMS object.
 </script>
+
+<svelte:head>
+    meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://unpkg.com/@staticcms/app@^3.0.0/dist/main.css" />
+    <script src="https://unpkg.com/@staticcms/app@^3.0.0/dist/static-cms-app.js"></script>
+</svelte:head>
